bc9d4914fbfc91a96736517ba5a42bc73f8f8862
Mkossek <magdak596@gmail.com>
2018-10-29 14:38:22 -0600

    First commit

---
+++ PsOC/Smart_Meter_PSoC6_Webserver.c
@@ -0,0 +1,385 @@
+/******************************************************************************
+* File Name: cy8ckit_062_demo.c
+*
+* Version 1.0
+*
+* This application demonstrates a PSoC 6 device hosting an http webserver
+* The PSoC 6 measures the voltage of the ambient light sensor on the CY8CKIT-028
+* It then displays that information on the webserver.
+* The PSoC 6 also controls the brightness of the RED led on the board. The brightness
+* can be controlled by the two capsense buttons, capsense slider, or webpage.
+*
+* Related Document: CE_Title.pdf
+*
+* Hardware Dependency: CY8CKIT_062_WiFi with CY8CKIT_028
+*
+*******************************************************************************
+* Copyright (2017), Cypress Semiconductor Corporation. All rights reserved.
+*******************************************************************************
+* This software, including source code, documentation and related materials
+* (“Software”), is owned by Cypress Semiconductor Corporation or one of its
+* subsidiaries (“Cypress”) and is protected by and subject to worldwide patent
+* protection (United States and foreign), United States copyright laws and
+* international treaty provisions. Therefore, you may use this Software only
+* as provided in the license agreement accompanying the software package from
+* which you obtained this Software (“EULA”).
+*
+* If no EULA applies, Cypress hereby grants you a personal, nonexclusive,
+* non-transferable license to copy, modify, and compile the Software source
+* code solely for use in connection with Cypress’s integrated circuit products.
+* Any reproduction, modification, translation, compilation, or representation
+* of this Software except as specified above is prohibited without the express
+* written permission of Cypress.
+*
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+* reserves the right to make changes to the Software without notice. Cypress
+* does not assume any liability arising out of the application or use of the
+* Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use in any products where a malfunction or
+* failure of the Cypress product may reasonably be expected to result in
+* significant property damage, injury or death (“High Risk Product”). By
+* including Cypress’s product in a High Risk Product, the manufacturer of such
+* system or application assumes all risk of such use and in doing so agrees to
+* indemnify Cypress against all liability.
+* *******************************************************************************/
+
+
+#include "Smart_Meter_PSoC6_Webserver.h"
+
+/******************************************************
+ *               Global Variable Definitions
+ ******************************************************/
+/* Holds usage data and time stamp*/
+static usage_t      usageData;
+
+/* Variables for server*/
+static wiced_http_server_t httpServer;
+
+/* ugui instance*/
+UG_GUI   gui;
+
+/*Global to hold display row position*/
+uint16_t row = TOP_DISPLAY;
+
+/*Flag to indicate is screen is ready for printing usage data*/
+uint8_t screenReady = 0;
+
+/*Pointers to webpages*/
+static START_OF_HTTP_PAGE_DATABASE(web_pages)
+    ROOT_HTTP_PAGE_REDIRECT("/apps/cy8ckit_062_demo/main.html"),
+    { "/apps/cy8ckit_062_demo/main.html","text/html",                WICED_RESOURCE_URL_CONTENT,  .url_content.resource_data  = &resources_apps_DIR_Smart_Meter_PSoC6_Webserver_DIR_main_html, },
+    { "/temp_report.html",               "text/html",                WICED_DYNAMIC_URL_CONTENT,   .url_content.dynamic_data   = {processUsageUpdate, 0 }, },
+    { "/images/favicon.ico",             "image/vnd.microsoft.icon", WICED_RESOURCE_URL_CONTENT,  .url_content.resource_data  = &resources_images_DIR_favicon_ico, },
+    { "/scripts/general_ajax_script.js", "application/javascript",   WICED_RESOURCE_URL_CONTENT,  .url_content.resource_data  = &resources_scripts_DIR_general_ajax_script_js, },
+    { "/styles/buttons.css",             "text/css",                 WICED_RESOURCE_URL_CONTENT,  .url_content.resource_data  = &resources_styles_DIR_buttons_css, },
+END_OF_HTTP_PAGE_DATABASE();
+
+
+void application_start( void )
+{
+    /*Maximum socket connections*/
+    uint16_t                maxSockets = MAX_SOCKETS;
+
+    /*Semaphore used for sleeping application thread*/
+    wiced_semaphore_t       semaphore;
+
+    /*Hold results from WICED API*/
+    wiced_result_t          result;
+
+    /*Indicates if there is data in DCT or not*/
+    wiced_bool_t*           device_configured;
+    /*Location for device_configured to point to when clearing DCT*/
+    wiced_bool_t            clearDCT = 0;
+
+    /*IP address for device when connected to another network*/
+    wiced_ip_address_t      ipv4Address;
+    uint32_t                ipAddress;
+    char                    buffer[50];
+
+    /* information on configuration access point*/
+    wiced_config_soft_ap_t*  configAp;
+
+    /* Initialize the device and WICED framework */
+    wiced_init( );
+
+    /*Initialize the semaphore used to sleep the application thread*/
+    wiced_rtos_init_semaphore( &semaphore );
+
+    /*Initialize the TFT LCD*/
+    Cy_TFT_Init();
+    /*Connect ugui display driver*/
+    UG_Init( &gui, Cy_TFT_displayDriver, 320, 240 );
+
+    /*Setup display for this demo*/
+    /*Fill screen with black*/
+    UG_FillScreen( C_BLACK );
+    UG_SetBackcolor( C_BLACK );
+    UG_SetForecolor( C_WHITE );
+    UG_FontSelect( &HEADING_FONT );
+
+    /*Print welcome message*/
+    UG_PutString( 10, row,  "CSU Meter Project");
+    /*Increment row for next line*/
+    row = (HEADING_FONT.char_height + EXTRA_FONT_SPACE);
+
+    /*Check if there is a network stored in DCT, if not display instructions*/
+    wiced_dct_read_lock( (void**) &device_configured, WICED_FALSE, DCT_WIFI_CONFIG_SECTION, OFFSETOF(platform_dct_wifi_config_t, device_configured), sizeof(wiced_bool_t) );
+
+    if ( *device_configured != WICED_TRUE)
+    {
+
+        /*Read out details of the configuration AP, for printing out*/
+        wiced_dct_read_lock( (void**) &configAp, WICED_FALSE, DCT_WIFI_CONFIG_SECTION, OFFSETOF(platform_dct_wifi_config_t, config_ap_settings), sizeof(wiced_config_soft_ap_t) );
+
+        /*Insert instruction on LCD for logging into WiCED network*/
+        UG_FontSelect( &TEXT_FONT );
+
+        UG_PutString( 0, row, "Using another device connect to");
+        row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+        UG_PutString( 0, row, "the following WiFi network:" );
+        row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+        sprintf(buffer, "SSID    :  %s", configAp->SSID.value);
+        UG_PutString( 0, row, buffer );
+        row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+        sprintf(buffer, "Password:  %s\n", configAp->security_key);
+        UG_PutString( 0, row, buffer );
+        row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+        UG_PutString( 0, row, "Open a web browser and go to " );
+        row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+        UG_PutString( 0, row, "http://192.168.0.1" );
+        row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+        UG_PutString( 0, row, "On the page click the Wi-Fi Setup");
+        row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+        UG_PutString( 0, row, "button. Select your WiFi network");
+        row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+        UG_PutString( 0, row, "type in password, press Connect");
+        row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+
+        /*Print message out UART as well*/
+        WPRINT_APP_INFO(("*******************************************************\n"));
+        WPRINT_APP_INFO(("Using another device connect to the following WiFi network \n\r"));
+        WPRINT_APP_INFO(("SSID    :  %s\n", configAp->SSID.value));
+        WPRINT_APP_INFO(("Password:  %s\n", configAp->security_key));
+        WPRINT_APP_INFO(("Open a web browser and go to  http://192.168.0.1 \n\r"));
+        WPRINT_APP_INFO(("On the page click the Wi-Fi Setup button. Select your WiFi network \n\r"));
+        WPRINT_APP_INFO(("type in the password, press connect \n\r"));
+        WPRINT_APP_INFO(("*******************************************************\n"));
+
+        /* Configure the device */
+        wiced_configure_device( NULL );
+
+        /*Unlock read of DCT*/
+        wiced_dct_read_unlock( configAp, WICED_FALSE );
+    }
+
+    /*Unlock read of DCT*/
+    wiced_dct_read_unlock( device_configured, WICED_FALSE );
+
+    /*Display message that we are connecting to the network entered on configuration webpage*/
+    UG_FontSelect( &TEXT_FONT );
+    UG_PutString( 0, row,  "Connecting please wait");
+    row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+
+    /* Bring up the network interface */
+    result = wiced_network_up( WICED_STA_INTERFACE, WICED_USE_EXTERNAL_DHCP_SERVER, NULL );
+    if(result == WICED_SUCCESS)
+    {
+        WPRINT_APP_INFO(("\n\r"));
+        WPRINT_APP_INFO( ( "Connected to network \n\r" ) );
+        UG_PutString( 0, row, "Connected to network");
+        row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+    }
+    else
+    {
+
+        /*Failed to connect to network try again*/
+        WPRINT_APP_INFO(("Could not connect to network \n\r"));
+        WPRINT_APP_INFO(("Hold SW2 for 1 sec to clear saved network \n\r" ) );
+        WPRINT_APP_INFO(("Then reset device and try again\n\r" ) );
+
+        /*Clear the screen*/
+        UG_FillScreen( C_BLACK );
+        row = TOP_DISPLAY;
+
+        UG_FontSelect( &HEADING_FONT );
+
+        /*Print welcome message*/
+        UG_PutString( 10, row,  "CSU Meter Project");
+        /*Increment row for next line*/
+        row = (HEADING_FONT.char_height + EXTRA_FONT_SPACE);
+
+        UG_FontSelect( &TEXT_FONT );
+
+        /*print failure messages*/
+        UG_PutString( 0, row, "Could not connect to network");
+        row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+        UG_PutString( 0, row, "Hold SW2 for 1 sec to clear saved");
+        row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+        UG_PutString( 0, row, "network, reset device and try again");
+        row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+
+        /*Wait for user to either press SW2 or reset the device*/
+        do
+        {
+            /*is the button pressed*/
+            if ( !wiced_gpio_input_get( WICED_BUTTON1 ) )
+            {
+                /*Delay for a second and check again*/
+                wiced_rtos_delay_milliseconds( 1000 );
+
+                if ( !wiced_gpio_input_get( WICED_BUTTON1 ) )
+                {
+                    /*Set flag to indicate DCT needs to be cleared*/
+                    clearDCT = 0;
+                    device_configured = &clearDCT;
+
+                    WPRINT_APP_INFO(( "Clearing saved network\n" ));
+                    UG_PutString( 0, row, "Clearing saved network");
+                    row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+
+                    /*Clear out the DCT*/
+                    wiced_dct_write( (void**) &device_configured, DCT_WIFI_CONFIG_SECTION, OFFSETOF(platform_dct_wifi_config_t, device_configured), sizeof(wiced_bool_t) );
+                    wiced_rtos_delay_milliseconds( 1000 );
+
+                    WPRINT_APP_INFO(( "Clearing done, reset the device\n" ));
+                    UG_PutString( 0, row, "Clearing done, reset the device");
+
+                    /*Wait for user to reset device*/
+                    while(1);
+                }
+            }
+
+        } while(1);
+    }
+
+    /*Print info that we are connected and starting the server*/
+    WPRINT_APP_INFO(("\n\r"));
+    WPRINT_APP_INFO(("Connecting to time server\n\r"));
+    WPRINT_APP_INFO(("This may take a minute\n\r"));
+    WPRINT_APP_INFO(("\n\r"));
+
+    UG_PutString( 0, row,  "Connecting to time server");
+    row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+    UG_PutString( 0, row,  "This may take a minute");
+    row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+
+    /* Disable roaming to other access points */
+    wiced_wifi_set_roam_trigger( -99 ); /* -99dBm ie. extremely low signal level */
+
+    /* Start automatic time synchronization and synchronize once every day. */
+    /* this is what takes a while */
+    sntp_start_auto_time_sync( 1 * DAYS );
+
+    /* Start web server to display current light sensor voltage  & PWM duty cycle */
+    wiced_http_server_start( &httpServer, 80, maxSockets, web_pages, WICED_STA_INTERFACE, DEFAULT_URL_PROCESSOR_STACK_SIZE );
+
+    /*Tell user we are ready to go*/
+    WPRINT_APP_INFO( ( "Website Ready \n\r" ) );
+
+    /*Display instructions on LCD*/
+    UG_FillScreen( C_BLACK );
+    UG_FontSelect( &HEADING_FONT );
+    row = TOP_DISPLAY;
+    UG_PutString( 10, row,  "CSU Meter Project");
+
+    UG_FontSelect( &TEXT_FONT );
+    row = (HEADING_FONT.char_height + EXTRA_FONT_SPACE);
+
+    /*Grab IP address to tell user which IP address to connect to*/
+    wiced_ip_get_ipv4_address(WICED_STA_INTERFACE, &ipv4Address);
+
+    /*Print message to connect to that ip address*/
+    ipAddress = (uint32_t)ipv4Address.ip.v4;
+    sprintf(buffer, "%u.%u.%u.%u",  (unsigned char) ( ( ipAddress >> 24 ) & 0xff ),
+                                    (unsigned char) ( ( ipAddress >> 16 ) & 0xff ),
+                                    (unsigned char) ( ( ipAddress >> 8 ) & 0xff ),
+                                    (unsigned char) ( ( ipAddress >> 0 ) & 0xff ));
+
+    UG_PutString( 0, row, "On a device connected to the");
+    row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+    UG_PutString( 0, row, "same network open a web browser");
+    row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+    UG_PutString( 0, row, "and go to: ");
+    UG_PutString( 120, row, buffer);
+    row += (TEXT_FONT.char_height + EXTRA_FONT_SPACE);
+
+    WPRINT_APP_INFO(("\n\r"));
+    WPRINT_APP_INFO(("*******************************************************\n"));
+    WPRINT_APP_INFO(("On a device connected the same network \n\r"));
+    WPRINT_APP_INFO(("Open a web browser and go to: \n\r"));
+    WPRINT_APP_INFO(("%u.%u.%u.%u\n", (unsigned char) ( ( ipAddress >> 24 ) & 0xff ),
+                                      (unsigned char) ( ( ipAddress >> 16 ) & 0xff ),
+                                      (unsigned char) ( ( ipAddress >> 8 )  & 0xff ),
+                                      (unsigned char) ( ( ipAddress >> 0 )  & 0xff ) ) );
+    WPRINT_APP_INFO(("\n\r"));
+    WPRINT_APP_INFO(("Webpage presents meter data in plain text \n\r"));
+    WPRINT_APP_INFO(("*******************************************************\n"));
+
+    UG_FontSelect( &HEADING_FONT );
+
+    UG_PutString( 0, 90, "Usage (kW):");
+
+
+    /*Indicate to threads it is okay to print data*/
+    screenReady = 1;
+
+    /* Sleep this thread */
+    wiced_rtos_get_semaphore( &semaphore, WICED_WAIT_FOREVER );
+}
+
+/*
+ * Updates usage information on the web page
+ */
+int32_t processUsageUpdate( const char* url_path, const char* url_parameters, wiced_http_response_stream_t* stream, void* arg, wiced_http_message_body_t* http_data )
+{
+    wiced_iso8601_time_t*   curr_time;
+    char                    tempCharBuffer[6];
+    int                     tempCharBufferLength;
+
+    UNUSED_PARAMETER( url_path );
+    UNUSED_PARAMETER( http_data );
+
+    usageData.usage = rand()%30+1;
+
+    /*make sure it is okay to print data*/
+    if(screenReady)
+    {
+        UG_FontSelect( &HEADING_FONT );
+
+        /*Print on LCD the usage data*/
+        UG_PutString( 230, 115, usageData.usage);
+    }
+
+    /* Get the current ISO8601 time */
+    wiced_time_get_iso8601_time( &usageData.timestamp );
+
+    curr_time = &usageData.timestamp;
+
+    /* Write the time to the web page */
+    wiced_http_response_stream_write_resource( stream, &resources_apps_DIR_Smart_Meter_PSoC6_Webserver_DIR_data_html_time_start );
+    wiced_http_response_stream_write( stream, curr_time->hour, sizeof(curr_time->hour)   +
+                                                     sizeof(curr_time->colon1) +
+                                                     sizeof(curr_time->minute) +
+                                                     sizeof(curr_time->colon2) +
+                                                     sizeof(curr_time->second) );
+    wiced_http_response_stream_write_resource( stream, &resources_apps_DIR_Smart_Meter_PSoC6_Webserver_DIR_data_html_time_end );
+
+    /* Write the date to the webpage */
+    wiced_http_response_stream_write_resource( stream, &resources_apps_DIR_Smart_Meter_PSoC6_Webserver_DIR_data_html_date_start );
+    wiced_http_response_stream_write(stream, curr_time->year, sizeof(curr_time->year)  +
+                                                    sizeof(curr_time->dash1) +
+                                                    sizeof(curr_time->month) +
+                                                    sizeof(curr_time->dash2) +
+                                                    sizeof(curr_time->day) );
+    wiced_http_response_stream_write_resource( stream, &resources_apps_DIR_Smart_Meter_PSoC6_Webserver_DIR_data_html_date_end );
+
+    /* Write the usage data to the webpage */
+    wiced_http_response_stream_write_resource( stream, &resources_apps_DIR_Smart_Meter_PSoC6_Webserver_DIR_data_html_temp_start );
+    tempCharBufferLength = sprintf(tempCharBuffer, "%d", usageData.usage);
+    wiced_http_response_stream_write(stream, tempCharBuffer, tempCharBufferLength );
+    wiced_http_response_stream_write_resource(  stream, &resources_apps_DIR_Smart_Meter_PSoC6_Webserver_DIR_data_html_temp_end);
+
+    return 0;
+}
---
+++ PsOC/Smart_Meter_PSoC6_Webserver.h
@@ -0,0 +1,95 @@
+/*****************************************************************************
+* File Name: CE222494_PSoC6_WICED_WiFi.h
+*
+* Description: This file contains the function prototypes and constants used in
+*  CE222494_PSoC6_WICED_WiFi.c. This driver is intended for AK4954A.
+*
+******************************************************************************
+* Copyright (2018), Cypress Semiconductor Corporation.
+******************************************************************************
+* This software is owned by Cypress Semiconductor Corporation (Cypress) and is
+* protected by and subject to worldwide patent protection (United States and
+* foreign), United States copyright laws and international treaty provisions.
+* Cypress hereby grants to licensee a personal, non-exclusive, non-transferable
+* license to copy, use, modify, create derivative works of, and compile the
+* Cypress Source Code and derivative works for the sole purpose of creating
+* custom software in support of licensee product to be used only in conjunction
+* with a Cypress integrated circuit as specified in the applicable agreement.
+* Any reproduction, modification, translation, compilation, or representation of
+* this software except as specified above is prohibited without the express
+* written permission of Cypress.
+*
+* Disclaimer: CYPRESS MAKES NO WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, WITH
+* REGARD TO THIS MATERIAL, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+* Cypress reserves the right to make changes without further notice to the
+* materials described herein. Cypress does not assume any liability arising out
+* of the application or use of any product or circuit described herein. Cypress
+* does not authorize its products for use as critical components in life-support
+* systems where a malfunction or failure may reasonably be expected to result in
+* significant injury to the user. The inclusion of Cypress' product in a life-
+* support systems application implies that the manufacturer assumes all risk of
+* such use and in doing so indemnifies Cypress against all charges. Use may be
+* limited by and subject to the applicable Cypress software license agreement.
+*****************************************************************************/
+
+#include <math.h>
+#include "cy_tft_display.h"
+#include "wiced.h"
+#include "http_server.h"
+#include "sntp.h"
+#include "gedday.h"
+#include "resources.h"
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @cond */
+/******************************************************
+ *                     Macros
+ ******************************************************/
+
+/******************************************************
+ *                    Constants
+ ******************************************************/
+#define MAX_SOCKETS                     (10u)                       /* Maximum number of sockets */
+#define TOP_DISPLAY                     (0u)                        /* Top of the display*/
+#define EXTRA_FONT_SPACE                (3u)                        /* Spacing between printed lines*/
+#define TEXT_FONT                       (FONT_8X12)                 /* Font for display text*/
+#define HEADING_FONT                    (FONT_8X14)                 /* Font for heading text */
+/******************************************************
+ *                   Enumerations
+ ******************************************************/
+
+
+/******************************************************
+ *                 Type Definitions
+ ******************************************************/
+
+/******************************************************
+ *                    Structures
+ ******************************************************/
+typedef struct
+{
+    int                     usage;
+    wiced_iso8601_time_t    timestamp;
+} usage_t;
+
+/******************************************************
+ *                 Global Variables
+ ******************************************************/
+
+/******************************************************
+ *               Function Declarations
+ ******************************************************/
+wiced_result_t  calculateUsage         ( void* arg );
+int32_t         processUsageUpdate       ( const char* url_path, const char* url_parameters, wiced_http_response_stream_t* stream, void* arg, wiced_http_message_body_t* http_data );
+
+/** @endcond */
+#ifdef __cplusplus
+} /*extern "C" */
+#endif
+
