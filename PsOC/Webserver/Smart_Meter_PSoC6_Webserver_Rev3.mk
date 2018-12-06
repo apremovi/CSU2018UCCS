c37f4734dbdcbc3d2c87c0d034179a7a6a97ba6d
Andrew Premovich <apremovi@uccs.edu>
2018-12-05 19:30:12 -0700

    UART code working

---
+++ PsOC/Webserver/Smart_Meter_PSoC6_Webserver_Rev3.mk
@@ -0,0 +1,31 @@
+#
+# $ Copyright Cypress Semiconductor $
+#
+
+NAME := App_Smart_Meter_PSoC6_Webserver
+
+$(NAME)_SOURCES    := Smart_Meter_PSoC6_Webserver.c \
+					  cy_tft_display.c
+
+$(NAME)_COMPONENTS := daemons/HTTP_server \
+                      daemons/device_configuration \
+                      protocols/SNTP \
+                      graphics/ugui \
+                      daemons/Gedday 
+
+$(NAME)_RESOURCES  := apps/Smart_Meter_PSoC6_Webserver/main.html \
+                      apps/Smart_Meter_PSoC6_Webserver/data.html \
+                      images/favicon.ico \
+                      styles/buttons.css \
+                      scripts/general_ajax_script.js \
+                      scripts/wpad.dat
+
+GLOBAL_DEFINES     := USE_SELF_SIGNED_TLS_CERT
+
+
+VALID_PLATFORMS    := CY8CKIT_062
+
+ifeq ($(PLATFORM),$(filter $(PLATFORM), CYW9MCU7X9N364))
+GLOBAL_DEFINES += APPLICATION_STACK_SIZE=10*1024 WICED_DISABLE_TLS
+endif
+GLOBAL_DEFINES := WICED_DISABLE_STDIO
